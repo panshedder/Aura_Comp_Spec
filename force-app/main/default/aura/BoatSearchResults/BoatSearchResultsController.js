2c20f8764e855ea016a2adaa60422afe29b71eb5
Francisco <pancholiu@hotmail.com>
2021-01-09 14:34:39 -0300

    Step 5: Highlight the selected boat

--- force-app/main/default/aura/BoatSearchResults/BoatSearchResultsController.js
+++ force-app/main/default/aura/BoatSearchResults/BoatSearchResultsController.js
@@ -4,5 +4,10 @@
     },
     doSearch: function (component, event, helper) {
         helper.onSearch(component, event.getParam("arguments").boatTypeId);
+    },
+    onBoatSelect: function (component, event, helper) {
+        let boatId = event.getParam("boatId");
+        component.set("v.selectedBoatId", boatId);
+
     }
 })
--- force-app/main/default/aura/BoatTile/BoatTileController.js
+++ force-app/main/default/aura/BoatTile/BoatTileController.js
@@ -1,5 +1,8 @@
 ({
-    myAction : function(component, event, helper) {
-
+    onBoatClick: function (component, event, helper) {
+        let boatId = event.getSource().get("v.name");
+        let eventClickBoat = component.getEvent("boatSelected");
+        eventClickBoat.setParams({ "boatId": boatId });
+        eventClickBoat.fire();
     }
 })
